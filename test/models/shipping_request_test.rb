805e12f7d9d98eebee13635042f68d800b64b37a
Rowan Cota <cota.rg@gmail.com>
2016-05-24 11:04:50 -0700

    brainstorming tests.

--- test/models/shipping_request_test.rb
+++ test/models/shipping_request_test.rb
@@ -5,5 +5,21 @@ class ShippingRequestTest < ActiveSupport::TestCase
   #   assert true
   # end
 
+  correctly identifies number of boxes needed for items
+  correctly identifies weight of a shipment
+  returns price estimates in form of carrier: price (as a hash)
+   - carrier should be brand and type, example: UPS_Ground
+
+  stores shipping requests
+
+  ensures default zipcode is supplied if none provided for shipper
+
+  give a weight get prices
+
+  contains an order_id
+  contains a destination zipcode
+  contains a record of which ship type chosen
+  contains a record of price of chosen ship type
   
+
 end
