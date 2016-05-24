d16eb3c20df1387d32b3e450fe314f5de6ef16da
Lisa Rolczynski <lisa.sano@gmail.com>
2016-05-24 14:08:00 -0700

    added origin_zip to yml fixture and added origin_zip to validation in shipping_request model

--- app/models/shipping_request.rb
+++ app/models/shipping_request.rb
@@ -1,5 +1,5 @@
 class ShippingRequest < ActiveRecord::Base
-validates :destination_zip, :number_of_items,  :estimates, :order_id, presence: true
+validates :origin_zip, :destination_zip, :number_of_items,  :estimates, :order_id, presence: true
 validates :destination_zip, length: { is: 5 }
 validates :number_of_items, numericality: { only_integer: true, greater_than: 0 }
 validates :order_id, uniqueness: true, numericality: { only_integer: true }
