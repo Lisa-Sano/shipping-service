8261595cddc4dfb4d8d8898d740acf00590e0707
Suzanne Harrison <harrison_family@hotmail.com>
2016-05-24 12:58:23 -0700

    added migration schipping reuqests.

--- db/migrate/20160524173524_create_shipping_requests.rb
+++ db/migrate/20160524173524_create_shipping_requests.rb
@@ -2,6 +2,15 @@ class CreateShippingRequests < ActiveRecord::Migration
   def change
     create_table :shipping_requests do |t|
 
+      t.string    "origin_zip", :default => "98103"
+      t.string    "destination_zip", null: false
+      t.integer   "number_of_items", null: false
+      t.string    "estimates", null: false
+      t.string    "chosen_type"
+      t.string    "tracking_info"
+      t.integer   "order_id", null: false
+
+
       t.timestamps null: false
     end
   end
