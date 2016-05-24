f7c72480d8cfdbc69b53a107d2b9c1fd17017169
Suzanne Harrison <harrison_family@hotmail.com>
2016-05-24 13:09:35 -0700

    added validatations to the shippingrequest model.

--- app/models/shipping_request.rb
+++ app/models/shipping_request.rb
@@ -1,4 +1,8 @@
 class ShippingRequest < ActiveRecord::Base
+validates: :destination_zip, :number_of_items,  :estimates, :order_id, presence: true
+validates: :destination_zip, length: { is: 5 }
+validates: :number_of_items, numericality: { only_integer: true, :greater_than 0 }
+validates: :order_id, uniqueness: true, numericality: { only_integer: true }
 
   # Box sides are measured in inches. Item weight is measured in oz.
   BOX_SIDE_DIMENSION = 12
