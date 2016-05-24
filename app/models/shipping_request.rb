eac9e93aee5a37c809ef5ce10428b522da2f408c
Rowan Cota <cota.rg@gmail.com>
2016-05-24 10:40:40 -0700

    added standard measurement constants to model.

--- app/models/shipping_request.rb
+++ app/models/shipping_request.rb
@@ -1,2 +1,8 @@
 class ShippingRequest < ActiveRecord::Base
+
+  # Box sides are measured in inches. Item weight is measured in pounds.
+  BOX_SIDE_DIMENSION = 12
+  ITEM_WEIGHT_STANDARD = 3
+
+
 end
