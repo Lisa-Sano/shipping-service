3f0cd0c692d21a78aca816fc5b0c6a7bfb86b709
Rowan Cota <cota.rg@gmail.com>
2016-05-24 12:56:19 -0700

    set a constant for items per box.

--- app/models/shipping_request.rb
+++ app/models/shipping_request.rb
@@ -3,6 +3,7 @@ class ShippingRequest < ActiveRecord::Base
   # Box sides are measured in inches. Item weight is measured in oz.
   BOX_SIDE_DIMENSION = 12
   ITEM_WEIGHT_STANDARD = 48
+  ITEMS_PER_BOX = 5
 
 
 end
