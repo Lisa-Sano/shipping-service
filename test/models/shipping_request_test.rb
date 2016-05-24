d2f2bf5f9b252fbea5648b9f6f7dee8d7336592c
Rowan Cota <cota.rg@gmail.com>
2016-05-24 16:04:12 -0700

    updated tests.

--- test/models/shipping_request_test.rb
+++ test/models/shipping_request_test.rb
@@ -3,8 +3,6 @@ require 'test_helper'
 class ShippingRequestTest < ActiveSupport::TestCase
 
   test "determines the correct number of boxes to use" do
-    # set up order_one, shipping_requests[:order_two], order_three in  the fixtures
-
     assert_equal 1, shipping_requests(:order_one).number_of_boxes
     assert_equal 1, shipping_requests(:order_two).number_of_boxes
     assert_equal 2, shipping_requests(:order_three).number_of_boxes
@@ -27,7 +25,6 @@ class ShippingRequestTest < ActiveSupport::TestCase
     new_request = ShippingRequest.new
     refute new_request.valid?
     assert_includes new_request.errors.keys, :order_id
-    # assert_raises(ArgumentError) { new_request.order_id }
   end
 
   test "contains a shipper zipcode" do
@@ -44,7 +41,6 @@ class ShippingRequestTest < ActiveSupport::TestCase
     new_request.origin_zip = nil
     refute new_request.valid?
     assert_includes new_request.errors.keys, :origin_zip
-    # assert_raises(ArgumentError) { @invalid_request.origin_zip }
   end
 
   test "contains a destination zip" do
@@ -56,37 +52,23 @@ class ShippingRequestTest < ActiveSupport::TestCase
     new_request = ShippingRequest.new
     refute new_request.valid?
     assert_includes new_request.errors.keys, :destination_zip
-    # assert_raises(ArgumentError) { @invalid_request.destination_zip }
   end
 
-  # test "when given an order, correctly derives number of boxes and weight" do
-  #   # test object?
-  #   
-  # end
-
-  # test "when given an order, after deriving weight and boxes, returns correct price estimates" do
-    # write a test object here
-    # assert_equal (estimate amount), testobject.estimates[key for which estimate we're checking]
-    # assert_equal (second estimate amount), testobject.estimates[key for second estimate]
-  # end
-
   test "when shipping request is finalized it contains a record of chosen ship type and price" do
-
-    # this needs to be refactored to specifically call the key used in this hash to identify shipper
     refute_nil shipping_requests(:order_one).chosen_type
     refute_nil shipping_requests(:order_two).chosen_type
   end
 
   test "when a shipping request is finalized it won't validate non-preferred shipper" do
-    # write an API call to our API here for a bad request
+    @our_invalid_request = ShippingRequest.new
 
-    assert_raises(ArgumentError) {shipping_requests(:our_bad_request).chosen_type}
+    assert_raises(ArgumentError) {@our_bad_request.chosen_type}
   end
 
   test "a valid shipping request saves on completion" do
     # write a request here.
     @our_request = shipping_requests(:order_one)
-    
+
     assert @our_request.valid?
     assert @our_request.save
   end
