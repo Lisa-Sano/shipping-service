9301eb4358c0b81eb455697e636dd8fe81129d35
Lisa Rolczynski <lisa.sano@gmail.com>
2016-05-25 15:18:19 -0700

    added country and id to controller test keys and added id to the json response on #index and #show

--- app/controllers/shipping_requests_controller.rb
+++ app/controllers/shipping_requests_controller.rb
@@ -3,14 +3,14 @@ class ShippingRequestsController < ApplicationController
     requests = ShippingRequest.all
 
     #render as JSON EXCEPT for the created/updated_at. You can also use ONLY instead of except.
-    render json: requests.as_json(except: [:id, :created_at, :updated_at])
+    render json: requests.as_json(except: [:created_at, :updated_at])
   end
 
   def show
-    request = ShippingRequest.find_by(order_id: params[:id])
+    request = ShippingRequest.find_by(id: params[:id])
 
     if request.is_a?(ShippingRequest) #could also be if request.present?
-      render json: request.as_json(except: [:id, :created_at, :updated_at])
+      render json: request.as_json(except: [:created_at, :updated_at])
     else
       render json: [], status: :not_found
     end
