be570c43fac01d56ec2fc2c02c2333edc81d75aa
Rowan Cota <cota.rg@gmail.com>
2016-05-25 14:02:56 -0700

    changed a comment.

--- app/controllers/shipping_requests_controller.rb
+++ app/controllers/shipping_requests_controller.rb
@@ -2,16 +2,19 @@ class ShippingRequestsController < ApplicationController
   def index
     requests = ShippingRequest.all
 
-    render json: requests.as_json(except: [:id, :created_at, :updated_at]) #render as JSON EXCEPT for the created/updated_at. You can also use ONLY instead of except.
+    #render as JSON EXCEPT for the created/updated_at. You can also use ONLY instead of except.
+    render json: requests.as_json(except: [:id, :created_at, :updated_at]) 
   end
 
   def show
     request = ShippingRequest.find_by(id: params[:id])
 
-    if request.is_a?(ShippingRequest) #could also be if pet.present?
+    if request.is_a?(ShippingRequest) #could also be if request.present?
       render json: request.as_json(except: [:id, :created_at, :updated_at])
     else
       render json: [], status: :no_content
     end
   end
+
+
 end
