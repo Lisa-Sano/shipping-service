884118e2683c822e3fa76b082bb834191f3af7d7
Lisa Rolczynski <lisa.sano@gmail.com>
2016-05-25 19:34:31 -0700

    created seeds to test out api calls in localhost

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+ShippingRequest.create(
+  destination_zip: '98107', 
+  number_of_items: 3, 
+  estimates: "[{'UPS Standard': '3936'}, {'UPS Overnight': '5550'}]",
+  order_id: 1)
+ShippingRequest.create(
+  destination_zip: '98117', 
+  number_of_items: 10, 
+  estimates: "[{'UPS Standard': '4321'}, {'UPS Overnight': '10712'}]",
+  order_id: 2)
+ShippingRequest.create(
+  destination_zip: '98252', 
+  number_of_items: 5, 
+  estimates: "[{'UPS Standard': '2560'}, {'UPS Overnight': '13700'}]",
+  order_id: 3)
