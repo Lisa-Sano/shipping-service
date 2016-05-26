28f2c46d80c16caf10bb2eecd1ae5abe92328268
Rowan Cota <cota.rg@gmail.com>
2016-05-26 09:50:17 -0700

    removing unneeded columns.

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160526013406) do
-
+ActiveRecord::Schema.define(version: 20160526164811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,8 +27,6 @@ ActiveRecord::Schema.define(version: 20160526013406) do
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
     t.string   "country",         default: "USA",   null: false
-    t.string   "requests"
-    t.string   "responses"
   end
 
 end
