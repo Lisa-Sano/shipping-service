26e41de1aa68ea4bfe79067cb2af4baf691d7a96
Lisa Rolczynski <lisa.sano@gmail.com>
2016-05-26 10:33:25 -0700

    updated the number_of_boxes method to return a hash with the key being box number and value being how many items are in the box

--- app/models/shipping_request.rb
+++ app/models/shipping_request.rb
@@ -1,3 +1,5 @@
+require 'active_shipping'
+
 class ShippingRequest < ActiveRecord::Base
   validates :origin_zip, :destination_zip, :number_of_items,  :estimates, :order_id, presence: true
   validates :destination_zip, length: { is: 5 }
@@ -11,7 +13,12 @@ class ShippingRequest < ActiveRecord::Base
   ITEM_WEIGHT_STANDARD = 48
   ITEMS_PER_BOX = 5
 
-  def package(order)
+  def package(number_of_items)
+    # package = ActiveShipping::Package.new(16, [12, 12, 12], units: :imperial)
+    # 16 is the weight
+    # array is box dimensions
+
+
     # this will create the stuff we need to calculate what the package will cost.
     # multiply stuff from params by constants
   end
@@ -43,16 +50,23 @@ class ShippingRequest < ActiveRecord::Base
     # this should be a way to retrieve a tracking number from shippers.
   end
 
-  def number_of_boxes
-    if number_of_items % ITEMS_PER_BOX != 0
-      return (number_of_items / ITEMS_PER_BOX) + 1
-    else
-      return (number_of_items / ITEMS_PER_BOX)
+  def number_of_boxes(number_of_items)
+    boxes = {}
+    counter = 0
+
+    until number_of_items < 5
+      counter += 1
+      boxes["box_#{counter}"] = 5
+      number_of_items -= 5
     end
+
+    boxes["box_#{counter + 1}"] = number_of_items
+
+    return boxes
   end
 
-  def weight
-    return number_of_items * ITEM_WEIGHT_STANDARD
+  def weight(items_in_box)
+    return items_in_box * ITEM_WEIGHT_STANDARD
   end
 
 end
