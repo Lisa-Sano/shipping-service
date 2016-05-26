e59098e29e7296ced29570f44672379bd8e8dff5
Lisa Rolczynski <lisa.sano@gmail.com>
2016-05-26 09:00:05 -0700

    removed uniqueness from model

--- app/models/shipping_request.rb
+++ app/models/shipping_request.rb
@@ -2,7 +2,7 @@ class ShippingRequest < ActiveRecord::Base
   validates :origin_zip, :destination_zip, :number_of_items,  :estimates, :order_id, presence: true
   validates :destination_zip, length: { is: 5 }
   validates :number_of_items, numericality: { only_integer: true, greater_than: 0 }
-  validates :order_id, uniqueness: true, numericality: { only_integer: true }
+  validates :order_id, numericality: { only_integer: true }
 
   serialize :estimates, JSON
 
